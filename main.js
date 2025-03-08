122c85b65c6a6e4df877c10290dc502191214574
kibretOG <166326041+kibretOG@users.noreply.github.com>
2025-03-07 19:03:05 -0800

    widft no drop

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const width = 900 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
 // Create SVG containers for both charts
-const svg1_RENAME = d3.select("#lineChart1") // If you change this ID, you must change it in index.html too
+const svg1_RENAME = d3.select("#lineChart1")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -18,87 +18,56 @@ const svg2_RENAME = d3.select("#lineChart2")
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// (If applicable) Tooltip element for interactivity
-// const tooltip = ...
-const dropdown = d3.select("body")
-    .insert("select", ":first-child") // Insert at the top
-    .attr("id", "seasonDropdown");
-
-// 2.a: LOAD...
+// 2.a: LOAD DATA
+let allData;
 d3.csv("final_form_data.csv").then(data => {
     data.forEach(d => {
         d.Event_Year = new Date(d.Event_Date).getFullYear();
     });
+    allData = data;
+    updateChart(getSelectedSeasons()); // Default to all seasons on load
+});
 
-    const seasons = [...new Set(data.map(d => d.Season))];
+function getSelectedSeasons() {
+    return Array.from(document.querySelectorAll(".season-checkbox:checked"))
+        .map(checkbox => checkbox.value);
+}
 
-    dropdown.selectAll("option")
-        .data(seasons)
-        .enter()
-        .append("option")
-        .text(d => d)
-        .attr("value", d => d);
+function updateChart(selectedSeasons) {
+    const filteredData = allData.filter(d => selectedSeasons.includes(d.Season));
+    const yearCounts = d3.rollup(filteredData, v => v.length, d => d.Event_Year);
+    const dataset = Array.from(yearCounts, ([year, count]) => ({ year, count }))
+        .sort((a, b) => a.year - b.year);
 
-    function updateChart(selectedSeason) {
-        const filteredData = data.filter(d => d.Season === selectedSeason);
-        const yearCounts = d3.rollup(filteredData, v => v.length, d => d.Event_Year);
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(dataset, d => d.year))
+        .range([0, width]);
 
-        const dataset = Array.from(yearCounts, ([year, count]) => ({ year, count }))
-            .sort((a, b) => a.year - b.year);
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset, d => d.count)])
+        .range([height, 0]);
 
-        const xScale = d3.scaleLinear()
-            .domain(d3.extent(dataset, d => d.year))
-            .range([0, width]);
+    svg1_RENAME.selectAll("*").remove();
 
-        const yScale = d3.scaleLinear()
-            .domain([0, d3.max(dataset, d => d.count)])
-            .range([height, 0]);
+    svg1_RENAME.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(xScale).tickFormat(d3.format("d")));
 
-        svg1_RENAME.selectAll("*").remove();
+    svg1_RENAME.append("g")
+        .call(d3.axisLeft(yScale));
 
-        svg1_RENAME.append("g")
-            .attr("transform", `translate(0,${height})`)
-            .call(d3.axisBottom(xScale).tickFormat(d3.format("d")));
+    const line = d3.line()
+        .x(d => xScale(d.year))
+        .y(d => yScale(d.count));
 
-        svg1_RENAME.append("g")
-            .call(d3.axisLeft(yScale));
+    svg1_RENAME.append("path")
+        .datum(dataset)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 2)
+        .attr("d", line);
+}
 
-        const line = d3.line()
-            .x(d => xScale(d.year))
-            .y(d => yScale(d.count));
-
-        svg1_RENAME.append("path")
-            .datum(dataset)
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr("stroke-width", 2)
-            .attr("d", line);
-
-    
-
-    }
-    updateChart(seasons[0]);
-    dropdown.on("change", function () {
-        updateChart(this.value);
-    });
-
-    // ==========================================
-    //         CHART 2 (if applicable)
-    // ==========================================
-
-    // 3.b: SET SCALES FOR CHART 2
-
-
-    // 4.b: PLOT DATA FOR CHART 2
-
-
-    // 5.b: ADD AXES FOR CHART 
-
-
-    // 6.b: ADD LABELS FOR CHART 2
-
-
-    // 7.b: ADD INTERACTIVITY FOR CHART 2
-
-
+d3.selectAll(".season-checkbox").on("change", function () {
+    updateChart(getSelectedSeasons());
 });
