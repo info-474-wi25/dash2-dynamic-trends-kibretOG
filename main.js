2e05c8213d0145c44023aed1af1d5dc0f8a6dbd2
kibretOG <166326041+kibretOG@users.noreply.github.com>
2025-03-14 17:02:57 -0700

    final dash

--- main.js
+++ main.js
@@ -4,70 +4,81 @@ const width = 900 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
 // Create SVG containers for both charts
-const svg1_RENAME = d3.select("#lineChart1")
+const svg1_RENAME = d3.select("#lineChart1") // If you change this ID, you must change it in index.html too
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-const svg2_RENAME = d3.select("#lineChart2")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
-
-// 2.a: LOAD DATA
-let allData;
 d3.csv("final_form_data.csv").then(data => {
     data.forEach(d => {
         d.Event_Year = new Date(d.Event_Date).getFullYear();
     });
-    allData = data;
-    updateChart(getSelectedSeasons()); // Default to all seasons on load
-});
 
-function getSelectedSeasons() {
-    return Array.from(document.querySelectorAll(".season-checkbox:checked"))
-        .map(checkbox => checkbox.value);
-}
+    const seasons = [...new Set(data.map(d => d.Season))];
 
-function updateChart(selectedSeasons) {
-    const filteredData = allData.filter(d => selectedSeasons.includes(d.Season));
-    const yearCounts = d3.rollup(filteredData, v => v.length, d => d.Event_Year);
-    const dataset = Array.from(yearCounts, ([year, count]) => ({ year, count }))
-        .sort((a, b) => a.year - b.year);
+    const dropdown = d3.select("#seasonDropdown");
+    dropdown.selectAll("option")
+        .data(seasons)
+        .enter()
+        .append("option")
+        .text(d => d)
+        .attr("value", d => d);
 
-    const xScale = d3.scaleLinear()
-        .domain(d3.extent(dataset, d => d.year))
-        .range([0, width]);
+    function updateChart(selectedSeason) {
+        const filteredData = data.filter(d => d.Season === selectedSeason);
+        const yearCounts = d3.rollup(filteredData, v => v.length, d => d.Event_Year);
 
-    const yScale = d3.scaleLinear()
-        .domain([0, d3.max(dataset, d => d.count)])
-        .range([height, 0]);
+        const dataset = Array.from(yearCounts, ([year, count]) => ({ year, count }))
+            .sort((a, b) => a.year - b.year);
 
-    svg1_RENAME.selectAll("*").remove();
+        const xScale = d3.scaleLinear()
+            .domain(d3.extent(dataset, d => d.year))
+            .range([0, width]);
 
-    svg1_RENAME.append("g")
-        .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(xScale).tickFormat(d3.format("d")));
+        const yScale = d3.scaleLinear()
+            .domain([0, d3.max(dataset, d => d.count)])
+            .range([height, 0]);
 
-    svg1_RENAME.append("g")
-        .call(d3.axisLeft(yScale));
+        svg1_RENAME.selectAll("*").remove();
 
-    const line = d3.line()
-        .x(d => xScale(d.year))
-        .y(d => yScale(d.count));
+        svg1_RENAME.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(xScale).tickFormat(d3.format("d")));
 
-    svg1_RENAME.append("path")
-        .datum(dataset)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 2)
-        .attr("d", line);
-}
+        svg1_RENAME.append("g")
+            .call(d3.axisLeft(yScale));
 
-d3.selectAll(".season-checkbox").on("change", function () {
-    updateChart(getSelectedSeasons());
+        const line = d3.line()
+            .x(d => xScale(d.year))
+            .y(d => yScale(d.count));
+
+        svg1_RENAME.append("path")
+            .datum(dataset)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 2)
+            .attr("d", line);
+
+            svg1_RENAME.append("text")
+            .attr("x", width / 2)
+            .attr("y", height + margin.bottom - 10)
+            .attr("text-anchor", "middle")
+            .text("Year");  
+
+        svg1_RENAME.append("text")
+            .attr("x", -height / 2)
+            .attr("y", -margin.left + 30)
+            .attr("transform", "rotate(-90)")
+            .attr("text-anchor", "middle")
+            .text("Incident Count");  
+    }
+
+    updateChart(seasons[0]);
+
+    dropdown.on("change", function () {
+        updateChart(this.value);
+    });
+
 });
